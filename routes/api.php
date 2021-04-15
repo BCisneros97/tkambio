fa3d1fc6643ede36ee53fa915ee933106de3d0ac
Benny Cisneros <benycisneros@gmail.com>
2021-04-15 16:18:51 -0500

    Autenticacion JWT

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TipoCambioController;
+use App\Http\Controllers\api\v1\AuthController;
+use App\Http\Controllers\api\v1\TipoCambioController;
 
 Route::group(['prefix' => 'v1'], function () {
-    Route::apiResource('tipo-cambio', TipoCambioController::class)
-        ->parameter('tipo-cambio', 'tipoCambio');
+    Route::group(['prefix' => 'auth'], function () {
+        Route::post('login', [AuthController::class, 'login']);
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::post('refresh', [AuthController::class, 'refresh']);
+        Route::post('me', [AuthController::class, 'me']);
+    });
+
+    Route::group(['middleware' => 'jwt'], function () {
+        Route::apiResource('tipo-cambio', TipoCambioController::class)
+            ->parameter('tipo-cambio', 'tipoCambio');
+    });
 });
 
 
